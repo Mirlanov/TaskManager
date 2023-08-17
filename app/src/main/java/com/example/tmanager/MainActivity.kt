f14c2ce04fc38c595503a0a11b5e4789fe939fea
Mirlanov <mirlanovdamir@gmail.com>
2023-08-17 17:19:46 +0600

    gf

--- app/src/main/java/com/example/tmanager/MainActivity.kt
+++ app/src/main/java/com/example/tmanager/MainActivity.kt
@@ -29,8 +29,9 @@ class MainActivity : AppCompatActivity() {
                 R.id.navigation_home,
                 R.id.navigation_dashboard,
                 R.id.navigation_notifications,
-                R.id.taskFragment,
-                R.id.navigation_profile
+                R.id.navigation_profile,
+                R.id.taskFragment
+
 
             )
         )
